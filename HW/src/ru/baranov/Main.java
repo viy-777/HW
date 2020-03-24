0010b89781a4cc84f80994f4a96f0d894910afba
viy-777 <viy-777@ya.ru>
2020-03-24 17:09:50 +0500

    f

--- HW/src/ru/baranov/Main.java
+++ HW/src/ru/baranov/Main.java
@@ -15,13 +15,28 @@ public class Main {
 
         System.out.println("Случайное число в диапазоне 0..100: " + i);
 
-        int numm;
+        int N = 0;                                    //ввод пользователем числа
         System.out.println("Введите число:");
         Scanner in = new Scanner(System.in);
-        numm = in.nextInt();
+        N = in.nextInt();
 
-        int a = 3;
-        int b = 4;
-        int c = a + b;
+        boolean gameover = true;
+
+        while (gameover == true) {
+            int dif = Math.abs((i - N));
+            if (N == i) {System.out.println("Вы угадали число!");
+            gameover = false;
+            break;}
+        else
+            {
+                if (dif <= 5) {System.out.println("Горячо!");
+                System.out.println("Введите число:");
+                N = in.nextInt();}
+                if (dif > 5) {
+                    System.out.println("Холодно!");
+                    System.out.println("Введите число:");
+                    N = in.nextInt();}
+            }
+        }
     }
 }
--- untitled1/src/ru/baranov/Main.java
+++ untitled1/src/ru/baranov/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	int a = 5;
 	int b = 6;
+	int c = a + b;
     }
 }
