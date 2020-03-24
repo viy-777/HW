94f4f5474d54a3a02f54d45ad1d662c83f0ea292
viy-777 <viy-777@ya.ru>
2020-03-24 22:51:44 +0500

    new changes

--- HW/src/ru/baranov/Main.java
+++ HW/src/ru/baranov/Main.java
@@ -13,26 +13,33 @@ public class Main {
         int i = random.nextInt(diff + 1);                        // генерирация целого числа, включая max
         i += min;
 
-        System.out.println("Случайное число в диапазоне 0..100: " + i);
+        //System.out.println("Случайное число в диапазоне 0..100: " + i);
 
-        int N = 0;                                    //ввод пользователем числа
-        System.out.println("Введите число:");
+        int num = 0;                                    //ввод пользователем числа
+        System.out.println("Введите число от 0 до 100 (для выхода введите '0'):");
         Scanner in = new Scanner(System.in);
-        N = in.nextInt();
+        num = in.nextInt();
 
         boolean gameover = true;
 
         while (gameover == true) {
-            int dif = Math.abs((i - N));
-            if (N == i) {System.out.println("Вы угадали число!");
-                         gameover = false;
-                         break;}
-        else
-            {
-                if (dif <= 5) {System.out.println("Горячо! Введите число:");
-                               N = in.nextInt();}
-                if (dif > 5) {System.out.println("Холодно! Введите число:");
-                              N = in.nextInt();}
+            int dif = Math.abs((i - num));
+            if (num == 0) {                               //принудительное завершение цикла
+                gameover = false;
+                break;
+            } else if (num == i) {                         //условие правильного ввода числа
+                System.out.println("Вы угадали число!");
+                gameover = false;
+                break;
+            } else {
+                if (dif <= 10) {
+                    System.out.println("Горячо! Введите число от 0 до 100 (для выхода введите '0'):"); //условие ввода числа, близкого к загаданному
+                    num = in.nextInt();
+                }
+                if (dif > 10) {
+                    System.out.println("Холодно! Введите число от 0 до 100 (для выхода введите '0'):"); //условие ввода числа, близкого к загаданном
+                    num = in.nextInt();
+                }
             }
         }
     }
